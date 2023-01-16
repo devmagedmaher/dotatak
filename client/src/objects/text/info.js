5597a0025ae6ce21491269e67c0bbebe08395645
Maged Maher <dev.magedmaher@gmail.com>
2023-01-16 19:52:24 +0200

    show dash charge power remaining

--- client/src/objects/text/info.js
+++ client/src/objects/text/info.js
@@ -9,7 +9,8 @@ export default class InfoText extends Text {
 
   update() {
     if (this.scene.hero?.player) {
-      const player = this.scene.hero.player
+      const hero = this.scene.hero
+      const player = hero.player
       this.setText(
         `${this.scene.room}`
         + `- ${player.isAdmin ? '($) ' : ''}`
@@ -17,6 +18,8 @@ export default class InfoText extends Text {
         + `${player?.score||0} `
         + `(${player.alive ? 'alive' : 'dead'}) `
         + `[${['rock', 'paper', 'scissors'][player.mode]}]`
+        + `\n\n`
+        + `dash charge: ${100 - Math.ceil(hero.dash / (hero.dashPower - hero.linearSpeed) * 100)}%`
       )
     }
   }
